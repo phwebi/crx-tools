f0d2bc8e11b0248719e370e50be83d3d54da0900
Phoebe <phoebe@Phoebes-MacBook-Pro.local>
2015-03-10 14:55:05 -0700

    Deleted test files

--- deploy_listener.js
+++
@@ -1,6 +0,0 @@
-var hookshot = require('hookshot');
-
-hookshot('refs/heads/master',
-  'git fetch origin && git checkout origin/master && bash crxmake.sh ./chrome/ inspector.pem')
-
-.listen(3000);
